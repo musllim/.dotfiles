5d2fb7e18a2d3c577cfd97b3d53417df28028a7a
muslimuwitondanishema <muslimuwitondanishema@gmail.com>
2024-07-25 07:14:45 +0000

    initial commit

---
+++ .bashrc
@@ -0,0 +1,8 @@
+# ~/.bashrc
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+[ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
+PS1='$ '
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+      require("lspconfig").astro.setup {}
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "eslint-lsp",
+        "typescript-language-server",
+        "tailwindcss-language-server",
+      },
+    },
+  },
+  --
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
